dca91eb89181a7610ab51a7370b66467322cb720
tesa307 <tesa307@gmail.com>
2019-01-30 21:01:13 +0100

    Upload

---
+++ UnitTestDemo/PurchaseTest/UnitTest1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UnitTestDemo;
+
+namespace PurchaseTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void PurchaseA_NoDiscount()
+        {
+
+            int motherBoardPC = 4;
+            int cpuPC = 1;
+            int winchesterPC = 4;
+
+            Purchase purchase = new Purchase();
+            int result = purchase.PurchaseA(motherBoardPC, cpuPC, winchesterPC);
+            int expected = 4 * 100 + 1 * 120 + 4 * 80;
+
+            Assert.AreEqual(result, expected);
+        }
+
+        [TestMethod]
+        public void PurchaseB_NoDiscount()
+        {
+
+            int motherBoardPC = 4;
+            int cpuPC = 1;
+            int winchesterPC = 4;
+            double increase = 1.1;
+
+            Purchase purchase = new Purchase();
+            int result = purchase.PurchaseB(motherBoardPC, cpuPC, winchesterPC);
+            int expected = (int)(4 * (100 * increase) + 1 * (120 * increase) + 4 * (80 * increase));
+
+            Assert.AreEqual(result, expected);
+        }
+
+        [TestMethod]
+        public void PurchaseC_NoDiscount()
+        {
+
+            int motherBoardPC = 4;
+            int cpuPC = 1;
+            int winchesterPC = 4;
+            double increase = 1.3;
+
+            Purchase purchase = new Purchase();
+            int result = purchase.PurchaseC(motherBoardPC, cpuPC, winchesterPC);
+            int expected = (int)(4 * (100 * increase) + 1 * (120 * increase) + 4 * (80 * increase));
+
+            Assert.AreEqual(result, expected);
+        }
+
+        [TestMethod]
+        public void Discount_None()
+        {
+            Purchase purchase = new Purchase();
+
+            int result = purchase.Discount(1000, 30);
+            int expected = 0;
+
+            Assert.AreEqual(result, expected);
+        }
+
+        [TestMethod]
+        public void Discount_2()
+        {
+            Purchase purchase = new Purchase();
+
+            double discount = 1.02;
+            int result = purchase.Discount(1000, 31);
+            int expected = (int)(1000 * discount);
+
+            Assert.AreEqual(result, expected);
+        }
+
+        [TestMethod]
+        public void Discount_3()
+        {
+            Purchase purchase = new Purchase();
+
+            double discount = 1.03;
+            int result = purchase.Discount(1000, 51);
+            int expected = (int)(1000 * discount);
+
+            Assert.AreEqual(result, expected);
+        }
+
+        [TestMethod]
+        public void Discount_5()
+        {
+            Purchase purchase = new Purchase();
+
+            double discount = 1.05;
+            int result = purchase.Discount(10100, 11);
+            int expected = (int)(10100 * discount);
+
+            Assert.AreEqual(result, expected);
+        }
+    }
+}
---
+++ UnitTestDemo/UnitTestDemo/Purchase.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestDemo
+{
+    public class Purchase
+    {
+        private const int motherBoard = 100;
+        private const int cpu = 120;
+        private const int winchester = 80;
+
+        private const double discount2 = 1.02;
+        private const double discount3 = 1.03;
+        private const double discount5 = 1.05;
+
+        public int PurchaseA(int motherBoardPC, int cpuPC, int winchesterPC)
+        {
+            int s = motherBoardPC * motherBoard + cpuPC * cpu + winchesterPC * winchester;
+            return s;
+        }
+
+        public int PurchaseB(int motherBoardPC, int cpuPC, int winchesterPC)
+        {
+            double increase = 1.1;
+            int s = (int)(motherBoardPC * (motherBoard * increase) + cpuPC * (cpu * increase) + winchesterPC * (winchester * increase));
+            return s;
+        }
+
+        public int PurchaseC(int motherBoardPC, int cpuPC, int winchesterPC)
+        {
+            double increase = 1.3;
+            int s = (int)(motherBoardPC * (motherBoard * increase) + cpuPC * (cpu * increase) + winchesterPC * (winchester * increase));
+            return s;
+        }
+
+        public int Discount(int s, int pc)
+        {
+            int sd = 0;
+
+            if (s > 10000)
+                sd = (int)(s * discount5);
+
+            if (sd == 0 && pc > 30)
+            {
+                if (pc <= 50)
+                    sd = (int)(s * discount2);
+                else
+                    sd = (int)(s * discount3);
+            }
+
+            return sd;
+
+        }
+
+    }
+}
